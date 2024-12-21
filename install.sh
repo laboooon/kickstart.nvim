c56760c9714f61f64414ddfc212b94e007845d63
was-get-sie-das-an <was-geht-sie-das-an@gmail.com>
2024-12-21 16:39:47 +0100

    tmux movement improved

--- install.sh
+++ install.sh
@@ -2,14 +2,29 @@ set -e
 
 sudo apt-get install -y ripgrep
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
+# NVIM
+
+#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+#sudo rm -rf /opt/nvim
+#sudo tar -C /opt -xzf nvim-linux64.tar.gz
+#rm nvim-linux64.tar.gz
+
+# TMUX
 
 cp tmux.conf ~/.tmux.conf
+#rm -rf ~/.config/tmux/plugins/catppuccin
+#mkdir -p ~/.config/tmux/plugins/catppuccin
+#git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+# BASHRC
+
 cp bashrc ~/.bashrc
 
 rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
+# fuzzy history search
+#rm -rf ~/.fzf
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
 
 echo "installed successfully"
