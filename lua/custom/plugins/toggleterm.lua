9e0f290db5d11292ecba53492747355b45f94e8f
Schierbach <schierbach.leon@secunet.de>
2026-03-31 14:32:45 +0200

    fixed shellredir for windows

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,17 +2,26 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   config = function()
-    if vim.fn.has("win32") == 1 then
-      vim.cmd [[let &shell = '"C:/Program Files/Git/bin/bash.exe"']]
-      vim.cmd [[let &shellcmdflag = '-s']]
+    if vim.fn.has("win32") then
+      if vim.fn.executable("bash") then
+        vim.cmd [[let &shell = '"C:/Program Files/Git/bin/bash.exe"']]
+        vim.cmd [[let &shellcmdflag = '-c']]
+        vim.cmd [[let &shellquote = '']]
+        vim.cmd [[let &shellxquote = '']]
+        vim.cmd [[let &shellredir = '> %s 2>&1']]
+        vim.cmd [[let &shellpipe = '> %s 2>&1']]
+        vim.cmd [[set shellslash]]
+      end
     end
 
     require("toggleterm").setup({
       open_mapping = '<C-_>',
       start_in_insert = true,
+      autochdir = false,
       direction = 'float',
       shell = vim.o.shell,
     })
+
   end,
 }
 
